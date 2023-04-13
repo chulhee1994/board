1afec1db702b166e919a233d86e380f7f7fe4fcd
김철희 <61037406+chulhee1994@users.noreply.github.com>
2023-04-13 21:11:07 +0900

    Add files via upload

---
+++ src/main/java/com/myspring/pro30/board/dao/BoardDAOImpl.java
@@ -0,0 +1,106 @@
+package com.myspring.pro30.board.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.myspring.pro30.board.vo.ArticleVO;
+import com.myspring.pro30.board.vo.ImageVO;
+
+
+@Repository("boardDAO")
+public class BoardDAOImpl implements BoardDAO {
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public List selectAllArticlesList() throws DataAccessException {
+		List<ArticleVO> articlesList = articlesList = sqlSession.selectList("mapper.board.selectAllArticlesList");
+		return articlesList;
+	}
+
+	
+	@Override
+	public int insertNewArticle(Map articleMap) throws DataAccessException {
+		int articleNO = selectNewArticleNO();
+		articleMap.put("articleNO", articleNO);
+		System.out.println(articleMap.toString());
+		sqlSession.insert("mapper.board.insertNewArticle",articleMap);
+		return articleNO;
+	}
+	
+	public int insertReply(Map articleMap) throws DataAccessException{
+		int articleNO = selectNewArticleNO();
+		articleMap.put("articleNO", articleNO);
+		sqlSession.insert("mapper.board.insert", articleMap);
+				return articleNO;
+		
+	}
+    
+	//���� ���� ���ε�
+	/*
+	@Override
+	public void insertNewImage(Map articleMap) throws DataAccessException {
+		List<ImageVO> imageFileList = (ArrayList)articleMap.get("imageFileList");
+		int articleNO = (Integer)articleMap.get("articleNO");
+		int imageFileNO = selectNewImageFileNO();
+		for(ImageVO imageVO : imageFileList){
+			imageVO.setImageFileNO(++imageFileNO);
+			imageVO.setArticleNO(articleNO);
+		}
+		sqlSession.insert("mapper.board.insertNewImage",imageFileList);
+	}
+	
+   */
+	
+	public void updateCnt(int articleNO) throws DataAccessException {
+		sqlSession.update("mapper.board.updateCnt",articleNO);
+	}
+	
+	
+	
+	@Override
+	public ArticleVO selectArticle(int articleNO) throws DataAccessException {
+		return sqlSession.selectOne("mapper.board.selectArticle", articleNO);
+	}
+
+	@Override
+	public void updateArticle(Map articleMap) throws DataAccessException {
+		sqlSession.update("mapper.board.updateArticle", articleMap);
+	}
+
+	@Override
+	public void deleteArticle(int articleNO) throws DataAccessException {
+		sqlSession.delete("mapper.board.deleteArticle", articleNO);
+		
+	}
+	
+	@Override
+	public List selectImageFileList(int articleNO) throws DataAccessException {
+		List<ImageVO> imageFileList = null;
+		imageFileList = sqlSession.selectList("mapper.board.selectImageFileList",articleNO);
+		return imageFileList;
+	}
+	
+	private int selectNewArticleNO() throws DataAccessException {
+		return sqlSession.selectOne("mapper.board.selectNewArticleNO");
+	}
+	
+	private int selectNewImageFileNO() throws DataAccessException {
+		return sqlSession.selectOne("mapper.board.selectNewImageFileNO");
+		
+	}
+
+	//제일 최신글 번호 반환
+	@Override
+	public int maxArticle() throws DataAccessException {
+		return sqlSession.selectOne("mapper.board.maxArticle");
+	}
+	
+	
+}
